17c6afc81209cfaae0094b0e5130d58340fef85e
Artem Iglikov <artem.iglikov@gmail.com>
2014-12-24 19:38:25 +0600

    adapt tests skipping to parallel testing

--- cms/service/EvaluationService.py
+++ cms/service/EvaluationService.py
@@ -47,11 +47,12 @@ from cms import ServiceCoord, get_service_shards
 from cms.io import Executor, PriorityQueue, QueueItem, TriggeredService, \
     rpc_method
 from cms.db import Session, SessionGen, Contest, Dataset, Submission, \
-    SubmissionResult, Task, UserTest, UserTestResult
+    SubmissionResult, Task, UserTest, UserTestResult, Evaluation
 from cms.service import get_submissions, get_submission_results, \
     get_datasets_to_judge
 from cmscommon.datetime import make_datetime, make_timestamp
 from cms.grading.Job import JobGroup
+from cms.grading.scoretypes import get_score_type
 
 
 logger = logging.getLogger(__name__)
@@ -1133,6 +1134,40 @@ class EvaluationService(TriggeredService):
             return super(EvaluationService, self).enqueue(
                 operation, priority, timestamp) > 0
 
+    def cancel_skippable_testcases_evaluations(self, dataset, submission_result):
+        """Check for testcases which can be skipped and
+           cancel corresponding operations.
+        """
+
+        known_outcomes = dict()
+        for evaluation in submission_result.evaluations:
+            known_outcomes[evaluation.testcase.codename] = \
+                evaluation.outcome
+
+        logger.info("outcomes: %s" % str(known_outcomes))
+        score_type = get_score_type(dataset=dataset)
+        testcases_to_skip = score_type.get_testcases_to_skip(
+            known_outcomes)
+        logger.info("skipping: %s" % str(testcases_to_skip))
+        for test_name in testcases_to_skip:
+            submission_result.evaluations += [Evaluation(
+                text='["Evaluation skipped because of previous fails"]',
+                outcome="0.0",
+                testcase=submission_result.dataset.testcases[test_name])]
+            operation = ESOperation(ESOperation.EVALUATION,
+                          submission_result.submission.id,
+                          dataset.id,
+                          test_name)
+            try:
+                self.dequeue(operation)
+            except KeyError:
+                pass  # Ok, the operation wasn't in the queue.
+            try:
+                self.get_executor().pool.ignore_operation(operation)
+            except LookupError:
+                pass  # Ok, the operation wasn't in the pool.
+
+
     @with_post_finish_lock
     def action_finished(self, data, plus, error=None):
         """Callback from a worker, to signal that is finished some
@@ -1232,10 +1267,19 @@ class EvaluationService(TriggeredService):
                 if job_success:
                     job_group.to_submission_evaluation(submission_result)
 
+                dataset = Dataset.get_from_id(dataset_id, session)
+
+                # Mark skippable testcases as skipped
+
+                self.cancel_skippable_testcases_evaluations(dataset,
+                                                       submission_result)
+
+                #FIX: some ignorable testcase still can be evaluated
+                #because worker become free before we ignore evaluation jobs
+
                 # Submission evaluation will be ended only when
                 # evaluation for each testcase is available.
 
-                dataset = Dataset.get_from_id(dataset_id, session)
                 if len(submission_result.evaluations) == \
                         len(dataset.testcases):
                     submission_result.set_evaluation_outcome()
