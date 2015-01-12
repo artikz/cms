ae1ac51c60e65852cfb48a82133ee6366007b05e
Artem Iglikov <artem.iglikov@gmail.com>
2015-01-12 08:10:06 +0600

    update C/C++ compilation options

--- cms/grading/__init__.py
+++ cms/grading/__init__.py
@@ -100,7 +100,7 @@ def get_compilation_commands(language, source_filenames, executable_filename,
         command = ["/usr/bin/gcc"]
         if for_evaluation:
             command += ["-DEVAL"]
-        command += ["-static", "-O2", "-Wl,--stack=67108864",
+        command += ["-static", "-O2",
                     "-o", executable_filename]
         command += source_filenames
         command += ["-lm"]
@@ -109,7 +109,7 @@ def get_compilation_commands(language, source_filenames, executable_filename,
         command = ["/usr/bin/g++"]
         if for_evaluation:
             command += ["-DEVAL"]
-        command += ["-static", "-O2", "-std=c++11", "-Wl,--stack=67108864",
+        command += ["-static", "-O2", "-std=c++11",
                     "-o", executable_filename]
         command += source_filenames
         commands.append(command)
